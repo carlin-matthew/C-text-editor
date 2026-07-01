41ce79914a0ca7c10b677f6a93e9bd444a56d8cb
carlin-matthew <107957239+carlin-matthew@users.noreply.github.com>
2026-06-30 21:44:58 -0500

    Small refactor to create framework for keyboard shortcuts. Removed echoing of input.

--- kilo.c
+++ kilo.c
@@ -7,16 +7,20 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data***/
 
 struct termios orig_termios;
+
+/*** terminal ***/
 
 void die(const char *s) {
 	perror(s);
 	exit(1);
 }
-
-/*** terminal ***/
 
 void disableRawMode() {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
@@ -38,20 +42,34 @@ void enableRawMode() {
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+/*** input ***/
+
+void editorProccessKeypress() {
+	char c = editorReadKey();
+
+	switch (c) {
+		case CTRL_KEY('q'):
+		exit(0);
+		break;
+	}
+}
+
 /*** init ***/
 
 int main() {
 	enableRawMode();
 
 	while (1) {
-		char c = '\0';
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-		if (iscntrl(c)) {
-			printf("%d\r\n", c);
-		} else {
-			printf("%d ('%c')\r\n", c, c);
-		}
-		if (c == 'q') break;
+		editorProccessKeypress();
 	}
 
 	return 0;
