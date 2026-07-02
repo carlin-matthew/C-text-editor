247a1ff09ca9e8e91dd334fab129c0ee732ea7ec
carlin-matthew <107957239+carlin-matthew@users.noreply.github.com>
2026-07-02 16:49:50 -0500

    Added start of vertical scrolling

--- kilo.c
+++ kilo.c
@@ -41,6 +41,7 @@ typedef struct erow {
 
 struct editorConfig {
 	int cx, cy;
+	int rowoff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -219,10 +220,20 @@ void abFree(struct abuf *ab) {
 
 /** output ***/
 
+void editorScroll() {
+	if (E.cy < E.rowoff) {
+		E.rowoff = E.cy;
+	}
+	if (E.cy >= E.rowoff + E.screenrows) {
+		E.rowoff = E.cy - E.screenrows + 1;
+	}
+}
+
 void editorDrawRows(struct abuf *ab) {
 	int y;
 	for (y = 0; y < E.screenrows; y++) {
-		if (y >= E.numrows) {
+		int filerow = y + E.rowoff;
+		if (filerow >= E.numrows) {
 			if (E.numrows == 0 && y == E.screenrows / 3) {
 				char welcome[80];
 				int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
@@ -238,9 +249,9 @@ void editorDrawRows(struct abuf *ab) {
 				abAppend(ab, "~", 1);
 			}
 		} else {
-			int len = E.row[y].size;
+			int len = E.row[filerow].size;
 			if (len > E.screencols) len = E.screencols;
-			abAppend(ab, E.row[y].chars, len);
+			abAppend(ab, E.row[filerow].chars, len);
 		}
 
 		abAppend(ab, "\x1b[K", 3);
@@ -251,6 +262,8 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorRefreshScreen() {
+	editorScroll();
+
 	struct abuf ab = ABUF_INIT;
 	
 	abAppend(&ab, "\x1b[?25l", 6);
@@ -336,6 +349,7 @@ void editorProccessKeypress() {
 void initEditor() {
 	E.cx = 0;
 	E.cy = 0;
+	E.rowoff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 
